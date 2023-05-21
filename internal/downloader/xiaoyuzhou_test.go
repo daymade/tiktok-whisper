08cbe9a5f076f5ff80cffddff5c74bbeb5ab3234
daymade <tiansheng@outlook.com>
2023-05-21 12:19:21 +0800

    fix invalid characters in path

--- internal/downloader/xiaoyuzhou_test.go
+++ internal/downloader/xiaoyuzhou_test.go
@@ -169,3 +169,27 @@ func Test_buildEpisodeFilePath(t *testing.T) {
 		})
 	}
 }
+
+func Test_validPath(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "normal",
+			args: args{path: "Vol.13 从职场作家到创办企业，她用IP思维追求人生梦想| 对话职场作家七芊.m4a"},
+			want: "Vol.13 从职场作家到创办企业，她用IP思维追求人生梦想- 对话职场作家七芊.m4a",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validPath(tt.args.path); got != tt.want {
+				t.Errorf("validPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
